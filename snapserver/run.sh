9d7c1840863ab980b4ffad07edd6dcc9b31ac16a
M. Menger <121636110+M4ttisM3nger@users.noreply.github.com>
2023-01-24 20:57:59 +0100

    Update run.sh

--- snapserver/run.sh
+++ snapserver/run.sh
@@ -15,11 +15,19 @@ declare tcp
 declare logging
 declare threads
 declare datadir
-declare test
-
-test=/usr/bin/snapserver
 
 config=/etc/snapserver.conf
+
+threads=$(bashio::config 'server_threads')
+streams=$(bashio::config 'streams|join(", ")')
+buffer=$(bashio::config 'buffer')
+codec=$(bashio::config 'codec')
+muted=$(bashio::config 'send_to_muted')
+sampleformat=$(bashio::config 'sampleformat')
+http=$(bashio::config 'http_enabled')
+datadir=$(bashio::config 'server_datadir')
+tcp=$(bashio::config 'tcp_enabled')
+logging=$(bashio::config 'logging_enabled')
 
 if ! bashio::fs.file_exists '/etc/snapserver.conf'; then
     touch /etc/snapserver.conf ||
@@ -27,8 +35,42 @@ if ! bashio::fs.file_exists '/etc/snapserver.conf'; then
 fi
 bashio::log.info "Populating snapserver.conf..."
 
+{
+    echo "[server]"
+    echo "threads = ${threads}"
+    
+    echo "[stream]"
+    echo "${streams}"
+    
+    echo "buffer = ${buffer}"
+    echo "codec = ${codec}"
+    echo "send_to_muted = = ${muted}"
+    echo "sampleformat = ${sampleformat}"
 
+
+    echo "[http]"
+    echo "enabled = ${http}"
+    echo "bind_to_address = ::"
+    echo "doc_root = ${datadir}"
+    
+    echo "[tcp]"
+    echo "enabled = ${tcp}" 
+    
+    echo "[logging]"
+    echo "debug = ${logging}"
+    
+
+# Stream bis and ter
+#if bashio::config.has_value 'stream_bis'; then
+#    stream_bis=$(bashio::config 'stream_bis')
+#    echo "${stream_bis}" >> "${config}"
+#fi
+#if bashio::config.has_value 'stream_ter'; then
+#    stream_ter=$(bashio::config 'stream_ter')
+#    echo "${stream_ter}" >> "${config}"
+#fi
+
+cat ${config}
 bashio::log.info "Starting SnapServer..."
 
-
-/usr/bin/snapserver #-c /etc/snapserver.conf
+/usr/bin/snapserver -c /etc/snapserver.conf
